e3c2ebb1efe575d0d5dd1a03e40bb8beccbc07aa
ricky raihan <rickyraihan83@gmail.com>
2024-05-27 11:36:59 +0700

    remove_undian

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,98 +23,6 @@ function Header() {
   const handleOpen = () => setOpen(!open);
   return (
     <header className=" sticky top-0 z-50">
-      <section className="absolute -bottom-14 md:-bottom-16 right-4 z-0 ">
-        <button
-          onClick={handleOpen}
-          className="bg-light-blue-700 rounded-full text-sm md:text-base text-white md:px-4 md:py-3 px-3 py-2  sticky "
-        >
-          Ikuti Undian Bulanan
-        </button>
-        <Dialog open={open} handler={handleOpen}>
-          <DialogHeader className="-mb-1">Ikuti Undian Bulanan</DialogHeader>
-
-          <DialogBody className="space-y-4">
-            <Input label="Nama" />
-            <Input label="Email" />
-            <Input label="Nomor Telepon" />
-            <Popover placement="bottom">
-              <PopoverHandler>
-                <Input
-                  label="Tanggal Pembelian"
-                  onChange={() => null}
-                  value={date ? format(date, "PPP") : ""}
-                />
-              </PopoverHandler>
-              <PopoverContent>
-                <DayPicker
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  showOutsideDays
-                  className="border-0"
-                  classNames={{
-                    caption:
-                      "flex justify-center py-2 mb-4 relative items-center",
-                    caption_label: "text-sm font-medium text-gray-900",
-                    nav: "flex items-center",
-                    nav_button:
-                      "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300",
-                    nav_button_previous: "absolute left-1.5",
-                    nav_button_next: "absolute right-1.5",
-                    table: "w-full border-collapse",
-                    head_row: "flex font-medium text-gray-900",
-                    head_cell: "m-0.5 w-9 font-normal text-sm",
-                    row: "flex w-full mt-2",
-                    cell: "text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-                    day: "h-9 w-9 p-0 font-normal",
-                    day_range_end: "day-range-end",
-                    day_selected:
-                      "rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white",
-                    day_today: "rounded-md bg-gray-200 text-gray-900",
-                    day_outside:
-                      "day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10",
-                    day_disabled: "text-gray-500 opacity-50",
-                    day_hidden: "invisible",
-                  }}
-                  components={{
-                    IconLeft: ({ ...props }) => (
-                      <ChevronLeftIcon
-                        {...props}
-                        className="h-4 w-4 stroke-2"
-                      />
-                    ),
-                    IconRight: ({ ...props }) => (
-                      <ChevronRightIcon
-                        {...props}
-                        className="h-4 w-4 stroke-2"
-                      />
-                    ),
-                  }}
-                />
-              </PopoverContent>
-            </Popover>
-            <Select label="Produk Yang Dibeli">
-              <Option>Aerox</Option>
-              <Option>NMAX</Option>
-              <Option>XMAX</Option>
-            </Select>
-          </DialogBody>
-          <DialogFooter>
-            {/* <Button
-              variant="text"
-              color="red"
-              onClick={handleOpen}
-              className="mr-1"
-            >
-              <span>Cancel</span>
-            </Button> */}
-            {/* <Button variant="gradient" color="green" onClick={handleOpen}>
-              <span>Confirm</span>
-            </Button> */}
-            <div>sss</div>
-          </DialogFooter>
-        </Dialog>
-      </section>
       {/* Info */}
       <section className="bg-[#112769] py-2 hidden md:block">
         <div className=" flex items-center justify-between max-w-6xl mx-auto">
@@ -205,6 +113,101 @@ function Header() {
     </header>
   );
 }
+
+// {
+//   /* <section className="absolute -bottom-14 md:-bottom-16 right-4 z-0 ">
+//         <button
+//           onClick={handleOpen}
+//           className="bg-light-blue-700 rounded-full text-sm md:text-base text-white md:px-4 md:py-3 px-3 py-2  sticky "
+//         >
+//           Ikuti Undian Bulanan
+//         </button>
+//         <Dialog open={open} handler={handleOpen}>
+//           <DialogHeader className="-mb-1">Ikuti Undian Bulanan</DialogHeader>
+
+//           <DialogBody className="space-y-4">
+//             <Input label="Nama" />
+//             <Input label="Email" />
+//             <Input label="Nomor Telepon" />
+//             <Popover placement="bottom">
+//               <PopoverHandler>
+//                 <Input
+//                   label="Tanggal Pembelian"
+//                   onChange={() => null}
+//                   value={date ? format(date, "PPP") : ""}
+//                 />
+//               </PopoverHandler>
+//               <PopoverContent>
+//                 <DayPicker
+//                   mode="single"
+//                   selected={date}
+//                   onSelect={setDate}
+//                   showOutsideDays
+//                   className="border-0"
+//                   classNames={{
+//                     caption:
+//                       "flex justify-center py-2 mb-4 relative items-center",
+//                     caption_label: "text-sm font-medium text-gray-900",
+//                     nav: "flex items-center",
+//                     nav_button:
+//                       "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300",
+//                     nav_button_previous: "absolute left-1.5",
+//                     nav_button_next: "absolute right-1.5",
+//                     table: "w-full border-collapse",
+//                     head_row: "flex font-medium text-gray-900",
+//                     head_cell: "m-0.5 w-9 font-normal text-sm",
+//                     row: "flex w-full mt-2",
+//                     cell: "text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+//                     day: "h-9 w-9 p-0 font-normal",
+//                     day_range_end: "day-range-end",
+//                     day_selected:
+//                       "rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white",
+//                     day_today: "rounded-md bg-gray-200 text-gray-900",
+//                     day_outside:
+//                       "day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10",
+//                     day_disabled: "text-gray-500 opacity-50",
+//                     day_hidden: "invisible",
+//                   }}
+//                   components={{
+//                     IconLeft: ({ ...props }) => (
+//                       <ChevronLeftIcon
+//                         {...props}
+//                         className="h-4 w-4 stroke-2"
+//                       />
+//                     ),
+//                     IconRight: ({ ...props }) => (
+//                       <ChevronRightIcon
+//                         {...props}
+//                         className="h-4 w-4 stroke-2"
+//                       />
+//                     ),
+//                   }}
+//                 />
+//               </PopoverContent>
+//             </Popover>
+//             <Select label="Produk Yang Dibeli">
+//               <Option>Aerox</Option>
+//               <Option>NMAX</Option>
+//               <Option>XMAX</Option>
+//             </Select>
+//           </DialogBody>
+//           <DialogFooter>
+//             <Button
+//               variant="text"
+//               color="red"
+//               onClick={handleOpen}
+//               className="mr-1"
+//             >
+//               <span>Cancel</span>
+//             </Button>
+//              <Button variant="gradient" color="green" onClick={handleOpen}>
+//               <span>Confirm</span>
+//             </Button>
+//             <div>sss</div>
+//           </DialogFooter>
+//         </Dialog>
+//       </section> */
+// }
 
 // const Dialogs = ({ isOpen, onClose }) => {
 //   if (!isOpen) return null;
